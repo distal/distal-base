de2cbfde9da09212e010e3e642a8094431341873
Martin Biely <martin.biely@epfl.ch>
2012-08-03 16:31:30 +0200

    copy some initial stuff from kryo-testing

---
+++ src/main/scala/ch/epfl/lsr/netty/channel/basichandlers.scala
@@ -0,0 +1,24 @@
+package ch.epfl.lsr.netty.channel
+
+import org.jboss.netty.channel.SimpleChannelHandler
+import org.jboss.netty.channel.{ ChannelHandlerContext, MessageEvent, ExceptionEvent, LifeCycleAwareChannelHandler }
+
+
+
+trait MessageReceivedHandler extends SimpleChannelHandler  { 
+
+  def messageReceived(ctx :ChannelHandlerContext, e :MessageEvent) 
+
+  override def exceptionCaught(ctx :ChannelHandlerContext, e :ExceptionEvent) { 
+    e.getCause.printStackTrace
+    e.getChannel.close
+  }
+}
+
+trait EmptyLifeCycleAwareChannelHandler extends LifeCycleAwareChannelHandler { 
+  def afterAdd(ctx :ChannelHandlerContext) {}
+  def afterRemove(ctx :ChannelHandlerContext) {}
+
+  def beforeAdd(ctx :ChannelHandlerContext) {}
+  def beforeRemove(ctx :ChannelHandlerContext) {}
+}
