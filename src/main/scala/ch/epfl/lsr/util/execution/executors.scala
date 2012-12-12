05d2df6b4403c9fb497028ac127e1f4d0be865dc
Martin Biely <martin.biely@epfl.ch>
2012-12-12 17:00:21 +0100

    bigger queues

--- src/main/scala/ch/epfl/lsr/util/execution/executors.scala
+++ src/main/scala/ch/epfl/lsr/util/execution/executors.scala
@@ -10,13 +10,13 @@ object Executors {
   // new ThreadPoolExecutor(2, defaultThreadCount, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue[Runnable](), ThreadFactories.newNamedThreadFactory(name))
 
   def newCachedOrderedThreadPoolExecutor(keyFunction :Runnable=>Object, name :String) = { 
-    new OrderedThreadPoolExecutor(0, Integer.MAX_VALUE, java.lang.Long.MAX_VALUE, TimeUnit.SECONDS, new LinkedBlockingQueue(1000), name) { 
+    new OrderedThreadPoolExecutor(0, Integer.MAX_VALUE, java.lang.Long.MAX_VALUE, TimeUnit.SECONDS, new LinkedBlockingQueue(10000), name) { 
        def getChildExecutorKey(task :Runnable) = keyFunction(task)
     }
   }
 
   def newFixedThreadPoolExecutor(nthreads :Int, keyFunction :Runnable=>Object, name :String) = { 
-    new OrderedThreadPoolExecutor(nthreads, nthreads, java.lang.Long.MAX_VALUE, TimeUnit.SECONDS, new LinkedBlockingQueue(1000), name) { 
+    new OrderedThreadPoolExecutor(nthreads, nthreads, java.lang.Long.MAX_VALUE, TimeUnit.SECONDS, new LinkedBlockingQueue(10000), name) { 
       def getChildExecutorKey(task :Runnable) = keyFunction(task)
     }
   }
