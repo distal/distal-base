84ba710c8500ab1f54caba7a3322ce7a28babe62
Martin Biely <martin.biely@epfl.ch>
2012-12-12 17:01:51 +0100

    multiple messages in one runnable in downpool

--- src/main/scala/ch/epfl/lsr/netty/execution/downpool.scala
+++ src/main/scala/ch/epfl/lsr/netty/execution/downpool.scala
@@ -5,27 +5,30 @@ import ch.epfl.lsr.netty.channel.ChannelSource
 import ch.epfl.lsr.netty.util.ChannelFutures
 import java.nio.channels.ClosedChannelException
 
-class DownStreamRunnable(val source :ChannelSource, msg :Any) extends Runnable { 
+class DownStreamRunnable(val source :ChannelSource, msgs :Seq[Any]) extends Runnable { 
+  def writeMessage(ch :Channel, msg :Any) { 
+    val future = Channels.write(ch, msg)
+    ChannelFutures.onCompleted(future) { 
+      future =>
+	assume(future.isDone)
+	val cause = future.getCause
+	if(cause!=null)
+	  println("Exception during write (in I/O thread): "+cause.getMessage+" while sending "+msg+" to "+ source.conn.remote)
+    }
+  }
+
   def run() { 
     try { 
-      source.getCurrentContext match { 
-	case Some(ctx) => 
-	  val ch = ctx.getChannel
-	//println("DP write: "+msg+" "+source)
-	  val future = Channels.write(ch, msg)
-	  ChannelFutures.onCompleted(future) { 
-	    future =>
-	      assume(future.isDone)
-	      val cause = future.getCause
-	      if(cause!=null)
-		println("Exception during write (in I/O thread): "+cause.getMessage+" while sending "+msg+" to "+ source.remoteLocation)
-	  }
+      source.getCurrentChannel match { 
+	case Some(ch) => 
+	  //println("DP write: "+msg+" "+source)
+	  msgs.foreach(m => writeMessage(ch, m))
 	case None => 
-	  throw new Exception("failed to write "+msg+" to "+ source.remoteLocation +" (context unavailable)")
+	  throw new Exception("failed to write "+msgs+" to "+ source.conn.remote +" (context unavailable)")
       }
     } catch { 
       case t :Throwable =>
-	println("DownStreamRunnable caught "+t.getMessage+" while sending "+msg+" to "+ source.remoteLocation)
+	println("DownStreamRunnable caught "+t.getMessage+" while sending "+msgs+" to "+ source.conn.remote)
 	t.printStackTrace
     }
   }
@@ -39,7 +42,7 @@ object InDownPool {
     
   private def getSource(runnable :Runnable) = runnable.asInstanceOf[DownStreamRunnable].source
   
-  def write(source :ChannelSource, msg :Any) { 
+  def write(source :ChannelSource, msg :Any*) { 
     downPool execute new DownStreamRunnable(source, msg)
   }
 }
