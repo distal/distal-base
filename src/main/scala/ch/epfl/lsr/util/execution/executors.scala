b7b48cec16fd0229a02c49de24a70919ce7c5302
Martin Biely <martin.biely@epfl.ch>
2013-10-28 12:28:46 +0100

    orderedpool/executors optimizations * no longer expose queue type in constructor (replaced by queuelength) * avoid scheduling ChildExecutors if already scheduled (under high load) * OrderedThreadPoolExecutor no longer isA ThreadPoolExecutor (but isA   Executor); now hasA (underlying) ThreadPool

--- src/main/scala/ch/epfl/lsr/util/execution/executors.scala
+++ src/main/scala/ch/epfl/lsr/util/execution/executors.scala
@@ -4,19 +4,19 @@ import java.util.concurrent.{ LinkedBlockingQueue, TimeUnit, ThreadPoolExecutor,
 import java.util.concurrent.{ Executors => JE }
 
 object Executors {
-  val defaultThreadCount = Runtime.getRuntime().availableProcessors()
+  val defaultThreadCount = 2*Runtime.getRuntime().availableProcessors()
 
   def newCachedThreadPoolExecutor(name :String) = JE.newCachedThreadPool(ThreadFactories.newNamedThreadFactory(name))
   // new ThreadPoolExecutor(2, defaultThreadCount, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue[Runnable](), ThreadFactories.newNamedThreadFactory(name))
 
-  def newCachedOrderedThreadPoolExecutor(keyFunction :Runnable=>Object, name :String) :OrderedThreadPoolExecutor = { 
-    new OrderedThreadPoolExecutor(defaultThreadCount, Integer.MAX_VALUE, java.lang.Long.MAX_VALUE, TimeUnit.SECONDS, new LinkedBlockingQueue(10000), name) { 
+  def newCachedOrderedThreadPoolExecutor(keyFunction :Runnable=>Object, name :String, mailboxSize :Int) :OrderedThreadPoolExecutor = {
+    new OrderedThreadPoolExecutor(defaultThreadCount, java.lang.Integer.MAX_VALUE, mailboxSize, 60L, TimeUnit.SECONDS, name) {
       def getChildExecutorKey(task :Runnable) = keyFunction(task)
     }
   }
 
-  def newFixedThreadPoolExecutor(nthreads :Int, keyFunction :Runnable=>Object, name :String) = { 
-    new OrderedThreadPoolExecutor(nthreads, nthreads, java.lang.Long.MAX_VALUE, TimeUnit.SECONDS, new LinkedBlockingQueue(10000), name) { 
+  def newFixedThreadPoolExecutor(nthreads :Int, keyFunction :Runnable=>Object, name :String, mailboxSize :Int) = {
+    new OrderedThreadPoolExecutor(nthreads, nthreads, mailboxSize, java.lang.Long.MAX_VALUE, TimeUnit.SECONDS, name) {
       def getChildExecutorKey(task :Runnable) = keyFunction(task)
     }
   }
--- src/main/scala/ch/epfl/lsr/util/execution/orderedPool.scala
+++ src/main/scala/ch/epfl/lsr/util/execution/orderedPool.scala
@@ -3,8 +3,27 @@ package ch.epfl.lsr.util.execution
 import java.util.concurrent._
 
 
-abstract class OrderedThreadPoolExecutor(corePoolSize :Int, maxPoolSize :Int, keepAlive:Long, timeUnit :TimeUnit, workQueue :BlockingQueue[Runnable], name :String) extends ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAlive, timeUnit, workQueue, ThreadFactories.newNamedThreadFactory(name)) { 
-  
+abstract class OrderedThreadPoolExecutor(corePoolSize :Int, maxPoolSize :Int, workerQueueSize :Int, keepAlive:Long, timeUnit :TimeUnit, name :String) extends Executor {
+
+  lazy val underlyingPool = {
+    val runnablesQueue = if(corePoolSize == maxPoolSize)
+      new LinkedBlockingDeque[Runnable]
+    else
+      new SynchronousQueue[Runnable]
+    val pool = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAlive, timeUnit, runnablesQueue, ThreadFactories.newNamedThreadFactory(name))
+    pool.setRejectedExecutionHandler(new RejectedExecutionHandler() {
+      def rejectedExecution(r :Runnable, exec :ThreadPoolExecutor) {
+        println("rejected q="+ pool.getQueue.size+" p="+pool.getPoolSize);
+        throw new RuntimeException("RejectedExecution should not happen.")
+        exec.getQueue.put(r)
+      }
+    })
+    pool
+  }
+
+  def beforeExecute(t :Thread, r :Runnable) = {}
+  def afterExecute(r :Runnable, t :Throwable) = {}
+
   val childExecutors = new ConcurrentHashMap[AnyRef, ChildExecutor]()
   
   def removeChildExecutorKey(key :AnyRef) { 
@@ -20,82 +39,92 @@ abstract class OrderedThreadPoolExecutor(corePoolSize :Int, maxPoolSize :Int, ke
     getChildExecutor(task).executeNext(task)    
   }
 
-  def executeInParentPool(task :ChildExecutor) { 
-    super.execute(task)
-  }
+  def getChildExecutor(task:Runnable) :ChildExecutor = {
+    import scala.collection.JavaConversions._
 
-  def getChildExecutor(task:Runnable) :ChildExecutor = { 
     val key = getChildExecutorKey(task)
     var executor = childExecutors.get(key)
-    if(executor == null) { 
-      executor = new ChildExecutor
+    if(executor == null) {
+      executor = new ChildExecutor(key)
       val oldExecutor = childExecutors.putIfAbsent(key, executor)
-      if(oldExecutor != null) { 
-	executor = oldExecutor
+      if(oldExecutor != null) {
+        executor = oldExecutor
       }
     }
     executor
   }
 
-  class ChildExecutor() extends Runnable with Executor { 
-    private val isRunning = new java.util.concurrent.atomic.AtomicBoolean(false)
-    private val q = new LinkedBlockingDeque[Runnable]()
-    @volatile 
+
+  class ChildExecutor(key :AnyRef) extends Runnable with Executor {
+    import ChildExecutorState._
+    private val state = new java.util.concurrent.atomic.AtomicInteger(IDLE)
+    private val q = new LinkedBlockingDeque[Runnable](workerQueueSize)
+    @volatile
     private var lastRun = now
 
     private def now :Long = System.currentTimeMillis
-    
-    def executeNext(task :Runnable) { 
-      q.offerFirst(task) 
 
-      if(!isRunning.get) { 
-	executeInParentPool(this)
-      }
-      
+    def ensureScheduledInParentPool = {
+      if(scheduleIfIdle)
+        underlyingPool.execute(this)
     }
 
-    def execute(task :Runnable) { 
-      q.offer(task) 
 
-      if(!isRunning.get) { 
-	executeInParentPool(this)
-      }
+    def executeNext(task :Runnable) {
+      q.putFirst(task)
+
+      ensureScheduledInParentPool
     }
 
-    private def alreadyRunning = isRunning.compareAndSet(false, true)
+    def execute(task :Runnable) {
+      q.put(task)
 
-    def run() { 
+      ensureScheduledInParentPool
+    }
+
+    private def startIfScheduled = state.compareAndSet(SCHEDULED, RUNNING)
+    private def scheduleIfIdle = state.compareAndSet(IDLE, SCHEDULED)
+    private def setIdle = { assert(state.get == RUNNING); state.set(IDLE) }
+
+    def run() {
+
       val ran = false
 
-      if(alreadyRunning) { 
-	try { 
-	  val currentThread = Thread.currentThread
-	  var thrown :Throwable = null
-	  var task :Runnable = null
-	  
-	  while({ task = q.poll; task != null}) { 
-	    
-	    beforeExecute(currentThread, task)
-	    try { 
-	      task.run
-	    } catch { 
-	      case t :Throwable => 
-		println("_____"+t)
-		thrown = t; throw t
-	    } finally { 
-	      afterExecute(task, thrown)
-	    }
-	  }
-	} finally { 
-	  isRunning.set(false)
-	  // reschedule for missed tasks
-	  if(q.peek !=null && !isRunning.get)
-	    executeInParentPool(this)
-	}
+      if(startIfScheduled) {
+       try {
+         val currentThread = Thread.currentThread
+         var thrown :Throwable = null
+         var task :Runnable = null
+
+         while({ task = q.poll; task != null}) {
+
+           beforeExecute(currentThread, task)
+           try {
+             task.run
+           } catch {
+             case t :Throwable =>
+              println("_____"+t)
+              thrown = t; throw t
+           } finally {
+             afterExecute(task, thrown)
+           }
+         }
+       } finally {
+         setIdle
+         // reschedule for missed tasks
+         if(q.peek !=null)
+           ensureScheduledInParentPool
+       }
       }
     }
     lastRun = now
   }
 
 
+}
+
+object ChildExecutorState  {
+  final val IDLE = 0
+  final val SCHEDULED = 1
+  final val RUNNING = 2
 }
